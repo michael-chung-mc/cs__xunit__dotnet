6971b433537d67bd76cf6b1c56a0ca8dd709bf05
michael-chung-mc <chung.michael.mc@gmail.com>
2023-09-01 17:14:21 -0400

    INIT multiples

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using wip__cs__xunit.src.Multiple;
+
+namespace wip__cs__xunit
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Multiple m = new();
+            Console.WriteLine(m.sumMultiple(3, 5, 1000));
+        }
+    }
+}
