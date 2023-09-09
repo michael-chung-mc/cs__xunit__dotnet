c0134906fe4452aaceb48cbfa03423932373c1bb
michael-chung-mc <chung.michael.mc@gmail.com>
2023-09-09 18:14:08 -0400

    SET matrix equality && c++ rule of 3

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -4,28 +4,101 @@ Matrix::Matrix(int rows, int columns)
 {
 	rnum = rows;
 	cnum = columns;
-	grid = new double* [rnum];
+	this->grid = new double* [rnum];
 	for (int i = 0; i < rnum; i++)
 	{
-		grid[i] = new double[cnum];
+		this->grid[i] = new double[cnum];
 	}
 	for (int i = 0; i < rnum; i++)
 	{
 		for (int j = 0; j < cnum; j++)
 		{
-			grid[i][j] = 0;
+			this->grid[i][j] = 0;
+		}
+	}
+}
+
+Matrix::Matrix(Matrix& other)
+{
+	//std::cout << "matrix copy ctor" << std::endl;
+	this->rnum = other.rnum;
+	this->cnum = other.cnum;
+	//std::cout << "this: " << rnum << cnum << " other:" << other.rnum << other.cnum << std::endl;
+	this->grid = new double* [rnum];
+	for (int i = 0; i < rnum; i++)
+	{
+		this->grid[i] = new double[cnum];
+	}
+	for (int i = 0; i < rnum; i++)
+	{
+		for (int j = 0; j < cnum; j++)
+		{
+			this->grid[i][j] = other.grid[i][j];
 		}
 	}
 }
 
 Matrix::~Matrix()
 {
+	//std::cout << "deleting matrix" << &grid << std::endl;
 	for (int i = 0; i < rnum; i++)
 	{
-		delete grid[i];
+		delete[] this->grid[i];
+		this->grid[i] = nullptr;
 	}
-	delete grid;
-	grid = nullptr;
+	delete[] this->grid;
+	this->grid = nullptr;
+}
+
+Matrix& Matrix::operator=(Matrix other)
+{
+	//std::cout << "matrix copy assign" << std::endl;
+	if (this == &other) return *this;
+	this->rnum = other.rnum;
+	this->cnum = other.cnum;
+	for (int i = 0; i < rnum; i++)
+	{
+		delete[] this->grid[i];
+		this->grid[i] = nullptr;
+	}
+	delete[] this->grid;
+	this->grid = nullptr;
+	this->grid = new double* [rnum];
+	for (int i = 0; i < rnum; i++)
+	{
+		this->grid[i] = new double[cnum];
+	}
+	for (int i = 0; i < rnum; i++)
+	{
+		for (int j = 0; j < cnum; j++)
+		{
+			this->grid[i][j] = 0;
+		}
+	}
+	return *this;
+}
+
+bool Matrix::operator==(const Matrix other)
+{
+	//std::cout << "matrix equality" << std::endl;
+	if (other.rnum != rnum || other.cnum != cnum)
+	{
+		return false;
+	}
+	for (int i = 0; i < rnum; i++)
+	{
+		for (int j = 0; j < cnum; j++)
+		{
+			if (other.grid[i][j] != this->grid[i][j])
+			{
+				return false;
+			}
+			//std::cout << &grid[i][j] << std::endl;
+			//std::cout << &other.grid[i][j] << std::endl;
+			//std::cout << "==" << std::endl;
+		}
+	}
+	return true;
 }
 
 bool Matrix::checkValid(int row, int column)
@@ -37,7 +110,7 @@ void Matrix::setRC(int row, int column, double value)
 {
 	if (checkValid(row, column))
 	{
-		grid[row][column] = value;
+		this->grid[row][column] = value;
 	}
 }
 
@@ -45,6 +118,6 @@ double Matrix::getRC(int row, int column)
 {
 	if (checkValid(row, column))
 	{
-		return grid[row][column];
+		return this->grid[row][column];
 	}
 }
--- src/matrix.h
+++ src/matrix.h
@@ -1,12 +1,14 @@
 class Matrix {
 public:
 	double **grid;
+	int rnum;
+	int cnum;
 	Matrix(int row, int column);
+	Matrix(Matrix& other);
 	~Matrix();
+	Matrix& operator=(const Matrix other);
+	bool operator==(const Matrix other);
 	bool checkValid(int row, int column);
 	void setRC(int row, int column, double value);
 	double getRC(int row, int column);
-private:
-	int rnum;
-	int cnum;
 };
