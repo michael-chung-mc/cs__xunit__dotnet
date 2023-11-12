5c0c46cad8fb6c423700f3770753473005ee0f35
michael-chung-mc <chung.michael.mc@gmail.com>
2023-11-12 17:26:56 -0500

    finish gilded rose kata

---
+++ lib/GildedRose-Refactoring-Kata/GildedRose.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace csharp
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+        public void UpdateSellIn(int argIndex) {
+            Items[argIndex].SellIn = Items[argIndex].SellIn - 1;
+        }
+        protected void IncreaseQuality(int argIndex) {
+            Items[argIndex].Quality = Items[argIndex].Quality +1 > 50 ? 50: Items[argIndex].Quality + 1;
+        }
+        public void DecreaseQuality(int argIndex) {
+            Items[argIndex].Quality = Items[argIndex].Quality -1 < 0 ? 0 : Items[argIndex].Quality-1;
+        }
+        public void UpdateQuality()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                var varName = Items[i].Name;
+                if (varName != "Sulfuras, Hand of Ragnaros") {
+                    if (varName != "Aged Brie" && varName != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        DecreaseQuality(i);
+                    }
+                    else
+                    {
+                        IncreaseQuality(i);
+                        if (Items[i].SellIn < 11)  { IncreaseQuality(i); }
+                        if (Items[i].SellIn < 6) { IncreaseQuality(i); }
+                    }
+                    UpdateSellIn(i);
+
+                    if (Items[i].SellIn < 0)
+                    {
+                        if (varName == "Backstage passes to a TAFKAL80ETC concert") {
+                            Items[i].Quality = 0;
+                        } else if (varName != "Aged Brie")
+                        {
+                            DecreaseQuality(i);
+                        } 
+                        else {
+                            IncreaseQuality(i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ lib/GildedRose-Refactoring-Kata/GildedRoseTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    [TestFixture]
+    public class GildedRoseTest
+    {
+        [Test]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("foo", Items[0].Name);
+        }
+
+        [Test]
+        public void texttest() {
+            IList<Item> Items = new List<Item>{
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49
+                },
+				// this conjured item does not work properly yet
+				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+            var app = new GildedRose(Items);
+            // Assert.AreEqual("+5 Dexterity Vest, 10, 20", Items[0]);
+        }
+    }
+}
